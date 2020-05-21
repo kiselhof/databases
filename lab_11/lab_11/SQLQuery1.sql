91de0c1962c861e28fd1bda11723385c675c7061
Anna <49332976+kiselhof@users.noreply.github.com>
2020-05-21 12:10:23 +0300

    Add files via upload

---
+++ lab_11/lab_11/SQLQuery1.sql
@@ -0,0 +1,24 @@
+-- Ѕƒ  студентов (факультет Ц специальность Ц группа Ц студент). 
+
+--DROP TABLE STUDENT;
+
+CREATE TABLE Faculty(
+id_faculty INT NOT NULL PRIMARY KEY,
+faculty_name NVARCHAR(MAX) NOT NULL);
+
+CREATE TABLE Speciality(
+id_speciality INT NOT NULL PRIMARY KEY,
+id_faculty INT NOT NULL,
+spec_name NVARCHAR(MAX) NOT NULL,
+FOREIGN KEY (id_faculty) REFERENCES Faculty (id_faculty) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE AcademicGroup(
+id_speciality INT NOT NULL,
+id_group INT NOT NULL PRIMARY KEY,
+group_numb INT NOT NULL,
+FOREIGN KEY (id_speciality) REFERENCES Speciality (id_speciality) ON UPDATE CASCADE ON DELETE CASCADE);
+
+CREATE TABLE Student
+(stud_name NVARCHAR(max) NOT NULL,
+ id_group INT NOT NULL,
+ FOREIGN KEY (id_group) REFERENCES AcademicGroup (id_group) ON UPDATE CASCADE ON DELETE CASCADE);
